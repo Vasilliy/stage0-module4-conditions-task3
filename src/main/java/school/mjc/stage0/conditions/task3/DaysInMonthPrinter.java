e39080d0db6545676f0da130d757d23ccbb0f11b
Vasilliy <Vasily.vvk@gmail.com>
2022-10-26 10:23:40 +0300

    Done

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,60 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int january = 31;
+        int february = 28;
+        int march = 31;
+        int april = 30;
+        int may = 31;
+        int june = 30;
+        int july = 31;
+        int august = 31;
+        int september = 30;
+        int october = 31;
+        int november = 30;
+        int december = 31;
+
+
+        if ( month==1){
+            System.out.println(january);
+        }
+        else if ( month==2){
+            System.out.println(february);
+        }
+        else if ( month==3){
+            System.out.println(march);
+        }
+        else if ( month==4){
+            System.out.println(april);
+        }
+        else if ( month==5){
+            System.out.println(may);
+        }
+        else if ( month==6){
+            System.out.println(june);
+        }
+        else if ( month==7){
+            System.out.println(july);
+        }
+        else if ( month==8){
+            System.out.println(august);
+        }
+        else if ( month==9){
+            System.out.println(september);
+        }
+        else if ( month==10){
+            System.out.println(october);
+        }
+        else if ( month==11){
+            System.out.println(november);
+        }
+        else if ( month==12){
+            System.out.println(december);
+        }
+
+        else {
+            System.out.println("wrong number!");
+        }
+        }
     }
-}
+
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+
+        int pr;
+
+        if (salary <= 10000 && salary > 0) {
+            pr = 15;
+            System.out.println((double) salary * (100 - pr) / 100);
+        } else if (salary > 10000 && salary <= 20000) {
+            pr = 18;
+            System.out.println((double) salary * (100 - pr) / 100);
+        } else if (salary > 20000) {
+            pr = 20;
+            System.out.println((double) salary * (100 - pr) / 100);
+        } else {
+
+            System.out.println("wrong input!");
+        }
     }
 }
